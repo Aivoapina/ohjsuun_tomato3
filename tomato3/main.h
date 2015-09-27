8f157293d4f2a3a96d19b7a6b7d9a0e1d37834ab
varjonea <anssi@localhost.localdomain>
2015-09-27 16:43:52 +0300

    korjasin projektin

---
+++ tomato3/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <QMainWindow>
+
+class main : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit main(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MAIN_H
