35a45bcc7db47e64a7de70127c299110ec99a933
Konsta Boman <konsta.boman@student.tut.fi>
2015-12-04 17:15:59 +0200

    infoa ruutuun

--- areenapeli/shopscene.cpp
+++ areenapeli/shopscene.cpp
@@ -82,8 +82,8 @@ void shopscene::on_ButtonBuy_clicked()
             emit tavara_ostettu();
         }
         else{
-            //ui->infoRuutu->clear();
-            //ui->infoRuutu->setText("Myy ensin vanha aseesi!");
+            ui->infoRuutu->clear();
+            ui->infoRuutu->setText("Myy ensin vanha aseesi!");
         }
     }
     else if( ui->treeWidget->currentItem()->parent()->text(0) == "Panssarit"){
@@ -93,8 +93,8 @@ void shopscene::on_ButtonBuy_clicked()
             emit tavara_ostettu();
         }
         else{
-            //ui->infoRuutu->clear();
-            //ui->infoRuutu->setText("Myy ensin vanha panssarisi!");
+            ui->infoRuutu->clear();
+            ui->infoRuutu->setText("Myy ensin vanha panssarisi!");
         }
     }
 
